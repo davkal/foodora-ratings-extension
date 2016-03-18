158c58c92c1ab1dcae927c0cb1b25921ee4e851f
David Kaltschmidt <david.kaltschmidt@gmail.com>
2016-03-18 10:02:50 +0100

    Style popup

--- src/popup.js
+++ src/popup.js
@@ -22,18 +22,25 @@ class App extends React.Component {
   }
 
   renderItem(item) {
-    return <div>{item.label}</div>;
+    const hasVariations = item.variations.length > 0;
+    return (
+      <div className="history-item" key={item.label}>
+        <div className="history-item-label">{item.label}</div>
+        {hasVariations
+          && <div className="history-item-variations">{item.variations.join(', ')}</div>}
+      </div>
+    );
   }
 
   render() {
     const items = this.state.history;
     return (
       <div className="app">
-        <div className="app-header">
-          <h1>Foodora Order History</h1>
-        </div>
-        <div className="app-content">
-          {items.map(item => this.renderItem(item))}
+        <div className="app-header">Foodora Order History</div>
+        <div className="app-section">
+          <div className="history">
+            {items.map(item => this.renderItem(item))}
+          </div>
         </div>
       </div>
     );
